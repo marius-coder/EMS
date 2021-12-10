a2306af801282f40cdb583c47893b6cc7444d99b
marius-coder <64585379+marius-coder@users.noreply.github.com>
2021-12-10 20:11:37 +0100

    Update

--- EMS_Backend/Classes/Building.py
+++ EMS_Backend/Classes/Building.py
@@ -15,17 +15,17 @@ class Building():
         self.heat_capacity = self.df_params["Value"][2]
         self.net_storey_height = self.df_params["Value"][3]
 
-        self.wand = {"Fläche" : self.df_thermal_hull.iloc[0,1],
+        self.wand = {"Flï¿½che" : self.df_thermal_hull.iloc[0,1],
                      "U-Wert" : self.df_thermal_hull["U-Wert"][0],
                      "f_T" : self.df_thermal_hull["Temperatur-Korrekturfaktor"][0],}
         self.wand["LT"] = self.LT("wand")
 
-        self.dach = {"Fläche" : self.df_thermal_hull.iloc[1,1],
+        self.dach = {"Flï¿½che" : self.df_thermal_hull.iloc[1,1],
                      "U-Wert": self.df_thermal_hull["U-Wert"][1],
                      "f_T": self.df_thermal_hull["Temperatur-Korrekturfaktor"][1]}
         self.dach["LT"] = self.LT("dach")
 
-        self.fussboden = {"Fläche" : self.df_thermal_hull.iloc[2,1],
+        self.fussboden = {"Flï¿½che" : self.df_thermal_hull.iloc[2,1],
                      "U-Wert": self.df_thermal_hull["U-Wert"][2],
                      "f_T": self.df_thermal_hull["Temperatur-Korrekturfaktor"][2]}
         self.fussboden["LT"] = self.LT("fussboden")
@@ -34,23 +34,23 @@ class Building():
 
     def LT(self, Bauteil:str):
         try:
-            return getattr(self, Bauteil)["Fläche"] * getattr(self, Bauteil)["U-Wert"] * getattr(self, Bauteil)["f_T"]
+            return getattr(self, Bauteil)["Flï¿½che"] * getattr(self, Bauteil)["U-Wert"] * getattr(self, Bauteil)["f_T"]
 
         except:
-            return getattr(self, Bauteil)["Fläche"] * getattr(self, Bauteil)["U-Wert"]
+            return getattr(self, Bauteil)["Flï¿½che"] * getattr(self, Bauteil)["U-Wert"]
 
     def insert_windows(self, u_f:float, ff_anteil:float):
-        self.window = {"Fläche" : self.wand["Fläche"] * ff_anteil,
+        self.window = {"Flï¿½che" : self.wand["Flï¿½che"] * ff_anteil,
                        "U-Wert" : u_f,
                        "ff_anteil" : ff_anteil}
 
-        self.wand = {"Fläche" : self.wand["Fläche"] - self.window["Fläche"],
+        self.wand = {"Flï¿½che" : self.wand["Flï¿½che"] - self.window["Flï¿½che"],
                      "U-Wert" : self.df_thermal_hull["U-Wert"][0],
                      "f_T" : self.df_thermal_hull["Temperatur-Korrekturfaktor"][0]}
 
-        self.wand_gesamt = {"Fläche" : self.wand["Fläche"] + self.window["Fläche"],
-                       "U-Wert" : (u_f * self.window["Fläche"] + self.wand["U-Wert"] * self.wand["Fläche"]) /\
-                           (self.window["Fläche"] + self.wand["Fläche"])}
+        self.wand_gesamt = {"Flï¿½che" : self.wand["Flï¿½che"] + self.window["Flï¿½che"],
+                       "U-Wert" : (u_f * self.window["Flï¿½che"] + self.wand["U-Wert"] * self.wand["Flï¿½che"]) /\
+                           (self.window["Flï¿½che"] + self.wand["Flï¿½che"])}
         
 building = Building("./data/building.xlsx")
 building_oib = Building("./data/building_oib_16linie.xlsx")
