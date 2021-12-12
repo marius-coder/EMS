3e3b9d283a663fe1fdad49826f8ea416fcf7765e
marius-coder <64585379+marius-coder@users.noreply.github.com>
2021-12-12 19:52:57 +0100

    Big Update

--- EMS_Backend/Classes/Model.py
+++ EMS_Backend/Classes/Model.py
@@ -5,20 +5,20 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 import numpy as np
-from Exercise_3 import Building
+from Building import Building
 
 class Model():
 
     def __init__(self, path:str):
         self.building = Building(path)
         self.df_usage = pd.read_csv("./data/usage_profiles.csv", encoding="cp1252")
-        self.ta = np.genfromtxt("./data/climate.csv", delimiter=";") #°C
-        self.qsolar = np.genfromtxt("./data/Solar_gains.csv") #W/m²
+        self.ta = np.genfromtxt("./data/climate.csv", delimiter=";") #Â°C
+        self.qsolar = np.genfromtxt("./data/Solar_gains.csv") #W/mÂ²
 
     def init_sim(self):
-        self.qi_winter = df_usage["Qi Winter W/m²"].to_numpy()
-        self.qi_sommer = df_usage["Qi Sommer W/m²"].to_numpy()
-        self.qi = self.qi_winter                                 #Interne Gewinne als Kombination von Sommer und Winter, fürs erste = QI_winter
+        self.qi_winter = df_usage["Qi Winter W/mÂ²"].to_numpy()
+        self.qi_sommer = df_usage["Qi Sommer W/mÂ²"].to_numpy()
+        self.qi = self.qi_winter                                 #Interne Gewinne als Kombination von Sommer und Winter, fï¿½rs erste = QI_winter
         self.ach_v = df_usage["Luftwechsel_Anlage_1_h"].to_numpy() #Air change per hour through ventilation
         self.ach_i = df_usage["Luftwechsel_Infiltration_1_h"].to_numpy() #Air change per hour through infiltration
         self.qv = np.zeros(8760)        #Ventilation losses
@@ -32,25 +32,26 @@ class Model():
         dt = self.ta[t] - self.ti[t]
         self.qt[t] = self.building.LT * dt
 
-    def calc_qv(self, t):       #Berechnung Lüftungsverluste
+    def calc_qv(self, t):       #Berechnung Lï¿½ftungsverluste
         dt = self.ta[t] - self.ti[t]
-        self.qv = self.building.volumen * (self.ach_i + self.ach_v) / 3600 * 1,2 * 1,01 * dt
+        self.qv = self.building.volumen * (self.ach_i + self.ach_v) / 3600 * 1.2 * 1.01 * dt
         
 
 
     def handle_losses(self, t):
-        if 
+        
         self.q_loss[t] = self.qv[t] + self.qt[t] + self.qi_winter[t] - self.qsolar[t]
 
 
 
     def handle_heating(self, t):
+        pass
 
 
 
 
     def handle_cooling(self, t):
-
+        pass
 
 
 
@@ -66,7 +67,7 @@ class Model():
             #Heizung:
             self.handle_heating(t)
             
-            #Kühlung:
+            #Kï¿½hlung:
             self.handle_cooling(t)
 
 
---
+++ EMS_Backend/Classes/Simulation.py
@@ -0,0 +1,68 @@
+﻿
+
+
+
+
+dic_geoData_in = {
+    "Adresse" : "Geifieng",
+    "Sondentiefe" : 100,
+    "Abstand der Sonden" :10
+    }
+
+class Simulation():
+
+    def __init__(self,bool_geothermal = False, bool_WP = False):
+         self.bool_geothermal = bool_geothermal
+         self.bool_PV = bool_PV
+         self.bool_battery = bool_battery
+         self.bool_districtHeating = bool_districtHeating
+         self.bool_heatPump = bool_heatPump
+         self.bool_solarPower = bool_solarPower
+         
+         self.building = Building("./data/building.xlsx")
+         self.df_usage = pd.read_csv("./data/usage_profiles.csv", encoding="cp1252")
+         self.ta = np.genfromtxt("./data/climate.csv", delimiter=";") #°C
+         self.qsolar = np.genfromtxt("./data/Solar_gains.csv") #W/m²
+         
+
+    def Setup_Simulation(self, dic_geoData_in = {}):
+        if self.bool_geothermal == True:
+            self.dic_geoData_out = Get_geoData(dic_geoData_in)
+
+
+        self.qi_winter = df_usage["Qi Winter W/m²"].to_numpy()
+        self.qi_sommer = df_usage["Qi Sommer W/m²"].to_numpy()
+        self.qi = self.qi_winter                                 #Interne Gewinne als Kombination von Sommer und Winter, f�rs erste = QI_winter
+        self.ach_v = df_usage["Luftwechsel_Anlage_1_h"].to_numpy() #Air change per hour through ventilation
+        self.ach_i = df_usage["Luftwechsel_Infiltration_1_h"].to_numpy() #Air change per hour through infiltration
+        self.qv = np.zeros(8760)        #Ventilation losses
+        self.qt = np.zeros(8760)        #transmission losses
+        self.ti = np.ones(8760) * 20        #indoor temperature
+        self.q_loss = np.zeros(8760)    #total thermal losses/gains
+        self.qh = np.zeros(8760)        #Heating demand
+        self.qc = np.zeros(8760)        #cooling demand
+
+            
+
+    def Simulate(self):
+        for hour in range(8760):
+
+
+
+
+
+            pass
+        pass
+
+
+
+
+
+model = Simulation()
+model.Setup_Simulation()
+
+
+
+
+
+
