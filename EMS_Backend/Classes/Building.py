001071a9495d06e103380d702505755bb8f98a0c
marius-coder <64585379+marius-coder@users.noreply.github.com>
2021-12-13 22:16:15 +0100

    Update Simulation und Bulding + extra files

--- EMS_Backend/Classes/Building.py
+++ EMS_Backend/Classes/Building.py
@@ -15,51 +15,41 @@ class Building():
         self.heat_capacity = self.df_params["Value"][2]
         self.net_storey_height = self.df_params["Value"][3]
 
-        self.wand = {"Fl�che" : self.df_thermal_hull.iloc[0,1],
+        self.wand = {"Fläche" : self.df_thermal_hull.iloc[0,1],
                      "U-Wert" : self.df_thermal_hull["U-Wert"][0],
                      "f_T" : self.df_thermal_hull["Temperatur-Korrekturfaktor"][0],}
-        self.wand["LT"] = self.LT("wand")
+        self.wand["LT"] = self.calc_LT("wand")
 
-        self.dach = {"Fl�che" : self.df_thermal_hull.iloc[1,1],
+        self.dach = {"Fläche" : self.df_thermal_hull.iloc[1,1],
                      "U-Wert": self.df_thermal_hull["U-Wert"][1],
                      "f_T": self.df_thermal_hull["Temperatur-Korrekturfaktor"][1]}
-        self.dach["LT"] = self.LT("dach")
+        self.dach["LT"] = self.calc_LT("dach")
 
-        self.fussboden = {"Fl�che" : self.df_thermal_hull.iloc[2,1],
+        self.fußboden = {"Fläche" : self.df_thermal_hull.iloc[2,1],
                      "U-Wert": self.df_thermal_hull["U-Wert"][2],
                      "f_T": self.df_thermal_hull["Temperatur-Korrekturfaktor"][2]}
-        self.fussboden["LT"] = self.LT("fussboden")
+        self.fußboden["LT"] = self.calc_LT("fußboden")
 
         self.volumen = self.gfa * self.net_storey_height
+        self.LT_WandGesamt = 0
 
-    def LT(self, Bauteil:str):
+    def calc_LT(self, Bauteil:str):
         try:
-            return getattr(self, Bauteil)["Fl�che"] * getattr(self, Bauteil)["U-Wert"] * getattr(self, Bauteil)["f_T"]
+            return getattr(self, Bauteil)["Fläche"] * getattr(self, Bauteil)["U-Wert"] * getattr(self, Bauteil)["f_T"]
 
         except:
-            return getattr(self, Bauteil)["Fl�che"] * getattr(self, Bauteil)["U-Wert"]
+            return getattr(self, Bauteil)["Fläche"] * getattr(self, Bauteil)["U-Wert"]
 
     def insert_windows(self, u_f:float, ff_anteil:float):
-        self.window = {"Fl�che" : self.wand["Fl�che"] * ff_anteil,
+        self.window = {"Fläche" : self.wand["Fläche"] * ff_anteil,
                        "U-Wert" : u_f,
                        "ff_anteil" : ff_anteil}
 
-        self.wand = {"Fl�che" : self.wand["Fl�che"] - self.window["Fl�che"],
+        self.wand = {"Fläche" : self.wand["Fläche"] - self.window["Fläche"],
                      "U-Wert" : self.df_thermal_hull["U-Wert"][0],
                      "f_T" : self.df_thermal_hull["Temperatur-Korrekturfaktor"][0]}
 
-        self.wand_gesamt = {"Fl�che" : self.wand["Fl�che"] + self.window["Fl�che"],
-                       "U-Wert" : (u_f * self.window["Fl�che"] + self.wand["U-Wert"] * self.wand["Fl�che"]) /\
-                           (self.window["Fl�che"] + self.wand["Fl�che"])}
+        self.wand_gesamt = {"Fläche" : self.wand["Fläche"] + self.window["Fläche"],
+                       "U-Wert" : (u_f * self.window["Fläche"] + self.wand["U-Wert"] * self.wand["Fläche"]) /\
+                           (self.window["Fläche"] + self.wand["Fläche"])}
         
-building = Building("./data/building.xlsx")
-building_oib = Building("./data/building_oib_16linie.xlsx")
-building_ph = Building("./data/building_ph.xlsx")
-
-building.insert_windows(1.7, 0.3)
-
-print(building.LT("wand_gesamt"))
-
-print(building.gfa)
-print(building_ph.df_params)
-
--- EMS_Backend/Classes/Simulation.py
+++ EMS_Backend/Classes/Simulation.py
@@ -1,6 +1,7 @@
 ﻿
-
-
+import pandas as pd
+import numpy as np
+from Building import Building
 
 
 dic_geoData_in = {
@@ -11,8 +12,8 @@ dic_geoData_in = {
 
 class Simulation():
 
-    def __init__(self,bool_geothermal = False, bool_WP = False):
-         self.bool_geothermal = bool_geothermal
+    def __init__(self,bool_geothermal = False, bool_WP = False, bool_PV = False, bool_battery = False, bool_districtHeating = False, bool_heatPump = False, bool_solarPower = False, ):
+         self.bool_geothermal = bool_geothermal 
          self.bool_PV = bool_PV
          self.bool_battery = bool_battery
          self.bool_districtHeating = bool_districtHeating
@@ -21,8 +22,8 @@ class Simulation():
          
          self.building = Building("./data/building.xlsx")
          self.df_usage = pd.read_csv("./data/usage_profiles.csv", encoding="cp1252")
-         self.ta = np.genfromtxt("./data/climate.csv", delimiter=";") #°C
-         self.qsolar = np.genfromtxt("./data/Solar_gains.csv") #W/m²
+         self.ta = np.genfromtxt("./data/climate.csv", delimiter=";", usecols = (1), skip_header = 1) #°C
+         self.qsolar = np.genfromtxt("./data/Solar_gains.csv") #W/m² Solar gains
          
 
     def Setup_Simulation(self, dic_geoData_in = {}):
@@ -30,22 +31,56 @@ class Simulation():
             self.dic_geoData_out = Get_geoData(dic_geoData_in)
 
 
-        self.qi_winter = df_usage["Qi Winter W/m²"].to_numpy()
-        self.qi_sommer = df_usage["Qi Sommer W/m²"].to_numpy()
+        self.qi_winter = self.df_usage["Qi Winter W/m²"].to_numpy()
+        self.qi_sommer = self.df_usage["Qi Sommer W/m²"].to_numpy()
         self.qi = self.qi_winter                                 #Interne Gewinne als Kombination von Sommer und Winter, f�rs erste = QI_winter
-        self.ach_v = df_usage["Luftwechsel_Anlage_1_h"].to_numpy() #Air change per hour through ventilation
-        self.ach_i = df_usage["Luftwechsel_Infiltration_1_h"].to_numpy() #Air change per hour through infiltration
+        self.ach_v = self.df_usage["Luftwechsel_Anlage_1_h"].to_numpy() #Air change per hour through ventilation
+        self.ach_i = self.df_usage["Luftwechsel_Infiltration_1_h"].to_numpy() #Air change per hour through infiltration
         self.qv = np.zeros(8760)        #Ventilation losses
         self.qt = np.zeros(8760)        #transmission losses
-        self.ti = np.ones(8760) * 20        #indoor temperature
+        self.ti = np.ones(8760) * 20    #indoor temperature
         self.q_loss = np.zeros(8760)    #total thermal losses/gains
         self.qh = np.zeros(8760)        #Heating demand
         self.qc = np.zeros(8760)        #cooling demand
-
+        self.cp_air = 0.34  # spez. Wärme kapazität Luft (Wh/m3K)
             
+    def calc_QV(self, t):
+        """Ventilation heat losses [W/m²BGF] at timestep t"""
+        dT = self.ta[t - 1] - self.ti[t - 1]
+        room_height = self.building.net_storey_height
+        # thermally effective air change
+        eff_airchange = self.ach_i[t] + self.ach_v[t]  # * M.VentilationSystem.share_cs * rel_ACH_after_heat_recovery
+
+        self.qv[t] = eff_airchange * room_height * self.cp_air * dT
+        print(self.qv[t])
+
+    def calc_QT(self, t):
+        """Transmission heat losses [W/m²BGF] at timestep t"""
+        dT = self.ta[t] - self.ti[t]
+        dT_boden = 6 - self.ti[t] #TODO: Annahme das der Boden konstant 6°C hat. Später durch genaueres ersetzen.
+        q_wand = abs(self.building.wand["LT"] * dT)
+        q_fußboden = abs(self.building.fußboden["LT"] * dT)
+        q_dach = abs(self.building.dach["LT"] * dT )
+        self.qt[t] = q_wand + q_fußboden + q_dach
+        print(self.qt[t])
+        
+    def handle_losses(self, t):
+        # determine losses
+        self.q_loss[t] = (self.qt[t] + self.qv[t]) + self.qsolar[t] + self.qi[t]
+        print(self.q_loss[t])
+        # determine indoor temperature after losses
+        self.ti[t] = self.TI_after_Q(self.TI[t - 1], self.q_loss[t], self.building.heat_capacity)
+        print(self.ti[t])
 
     def Simulate(self):
         for hour in range(8760):
+
+            self.calc_QT(hour)
+            self.calc_QV(hour)
+            self.handle_losses(hour)
+
+
+
 
 
 
@@ -60,6 +95,7 @@ class Simulation():
 
 model = Simulation()
 model.Setup_Simulation()
+model.Simulate()
 
 
 
