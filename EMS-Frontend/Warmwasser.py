1558a198b9658421881af54e1c55fa46b2b480dd
marius-coder <64585379+marius-coder@users.noreply.github.com>
2021-12-28 23:15:09 +0100

    Update Warmwasser GUI

--- EMS-Frontend/Warmwasser.py
+++ EMS-Frontend/Warmwasser.py
@@ -13,14 +13,14 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_Form(object):
+class Ui_Form(QMainWindow):
 
     def layout_widgets(self, Form):
         return (Form.itemAt(i) for i in range(Form.count()))
 
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(1500, 250)
+        Form.resize(1350, 550)
         Form.setStyleSheet("QSlider::groove:horizontal\n"
                         "{\n"
                         "border: 1px solid #bbb;\n"
@@ -77,58 +77,119 @@ class Ui_Form(object):
                         "}")
 
         #Input
-        self.radioButton_Person = QtWidgets.QRadioButton(Form)
-        self.radioButton_Person.setGeometry(QtCore.QRect(1300, 120, 82, 17))
-        self.radioButton_Person.setObjectName("radioButton_Person")
-        self.radioButton_Person.setText("Liter/Person")
-        self.radioButton_m2 = QtWidgets.QRadioButton(Form)
-        self.radioButton_m2.setGeometry(QtCore.QRect(1300, 137, 82, 16))
-        self.radioButton_m2.setObjectName("radioButton_m2")
-        self.radioButton_m2.setText("Liter/m2")
-        self.doubleSpinBox_WWValue = QtWidgets.QDoubleSpinBox(Form)
-        self.doubleSpinBox_WWValue.setGeometry(QtCore.QRect(1230, 125, 62, 22))
-        self.doubleSpinBox_WWValue.setObjectName("doubleSpinBox_WWValue")
-        self.doubleSpinBox_WWValue.setRange(0,1000)
-        self.radioButton_Person.toggled['bool'].connect(self.radioButton_m2.repaint) # type: ignore
-        self.radioButton_m2.toggled['bool'].connect(self.radioButton_Person.repaint) # type: ignore
+        
         
 
         #Output der Summe
+        #Stündliche Summe
         self.lineEdit = QtWidgets.QLineEdit(Form)
-        self.lineEdit.setGeometry(QtCore.QRect(1230, 190, 40, 20))
+        self.lineEdit.setGeometry(QtCore.QRect(1230, 220, 40, 20))
         self.lineEdit.setReadOnly(True)
         self.lineEdit.setObjectName("lineEdit")
+        #Monatliche Summe
+        self.lineEdit_sumMonth = QtWidgets.QLineEdit(Form)
+        self.lineEdit_sumMonth.setGeometry(QtCore.QRect(620, 470, 40, 20))
+        self.lineEdit_sumMonth.setReadOnly(True)
+        self.lineEdit_sumMonth.setObjectName("lineEdit_sumMonth")
 
-        #Auswählen bzw. erstellen eines neuen Profils
+        #Auswahl Profilname
+        self.label_Profil = QtWidgets.QLabel(Form)
+        self.label_Profil.setGeometry(QtCore.QRect(730, 255, 200, 30))
+        self.label_Profil.setText("Eingabe Profilname")
         self.lineEdit_Profil = QtWidgets.QLineEdit(Form)
-        self.lineEdit_Profil.setGeometry(QtCore.QRect(1230, 20, 81, 20))
+        self.lineEdit_Profil.setGeometry(QtCore.QRect(730, 280, 150, 20))
         self.lineEdit_Profil.setObjectName("lineEdit_Profil")
+
+        #Combobox für Benutzerprofile
+        self.label_AuswahlProfil = QtWidgets.QLabel(Form)
+        self.label_AuswahlProfil.setGeometry(QtCore.QRect(730, 300, 200, 30))
+        self.label_AuswahlProfil.setText("Auswahl benutzerdefinierter Profile")
         self.comboBox_SelectProfile = QtWidgets.QComboBox(Form)
-        self.comboBox_SelectProfile.setGeometry(QtCore.QRect(1230, 45, 69, 22))
+        self.comboBox_SelectProfile.setGeometry(QtCore.QRect(730, 325, 150, 22))
         self.comboBox_SelectProfile.setObjectName("comboBox_SelectProfile")
+
+        #Profil speichern
         self.pushButton_SaveProfile = QtWidgets.QPushButton(Form)
-        self.pushButton_SaveProfile.setGeometry(QtCore.QRect(1229, 70, 75, 23))
+        self.pushButton_SaveProfile.setGeometry(QtCore.QRect(900, 278, 75, 23))
         self.pushButton_SaveProfile.setObjectName("pushButton_SaveProfile")
         self.pushButton_SaveProfile.setText("Save Profile")
+
+        #Profil löschen
         self.pushButton_DeleteProfile = QtWidgets.QPushButton(Form)
-        self.pushButton_DeleteProfile.setGeometry(QtCore.QRect(1320, 70, 75, 23))
+        self.pushButton_DeleteProfile.setGeometry(QtCore.QRect(900, 323, 75, 23))
         self.pushButton_DeleteProfile.setObjectName("pushButton_DeleteProfile")
         self.pushButton_DeleteProfile.setText("Delete Profile")
+
+        #Combobox für Standardprofile
+        self.label_DefaultProfil = QtWidgets.QLabel(Form)
+        self.label_DefaultProfil.setGeometry(QtCore.QRect(730, 345, 200, 30))
+        self.label_DefaultProfil.setText("Auswahl default Profile")
+        self.comboBox_DefaultProfile = QtWidgets.QComboBox(Form)
+        self.comboBox_DefaultProfile.setGeometry(QtCore.QRect(730, 370, 150, 22))
+        self.comboBox_DefaultProfile.setObjectName("comboBox_DefaultProfile")
+
+        #Input Warmwasserverbrauch
+        self.label_WWVerbrauch = QtWidgets.QLabel(Form)
+        self.label_WWVerbrauch.setGeometry(QtCore.QRect(730, 390, 200, 30))
+        self.label_WWVerbrauch.setText("Eingabe Warmwasserverbrauch")
+        self.doubleSpinBox_WWValue = QtWidgets.QDoubleSpinBox(Form)
+        self.doubleSpinBox_WWValue.setGeometry(QtCore.QRect(730, 415, 62, 22))
+        self.doubleSpinBox_WWValue.setObjectName("doubleSpinBox_WWValue")
+        self.doubleSpinBox_WWValue.setRange(0,1000)
+        self.radioButton_Person = QtWidgets.QRadioButton(Form)
+        self.radioButton_Person.setGeometry(QtCore.QRect(800, 410, 82, 17))
+        self.radioButton_Person.setObjectName("radioButton_Person")
+        self.radioButton_Person.setText("Liter/Person")
+        self.radioButton_m2 = QtWidgets.QRadioButton(Form)
+        self.radioButton_m2.setGeometry(QtCore.QRect(800, 427, 82, 16))
+        self.radioButton_m2.setObjectName("radioButton_m2")
+        self.radioButton_m2.setText("Liter/m2")      
+        self.radioButton_Person.toggled['bool'].connect(self.radioButton_m2.repaint) 
+        self.radioButton_m2.toggled['bool'].connect(self.radioButton_Person.repaint) 
+
+        #Fertig mit Eingabe
         self.pushButton_UseProfile = QtWidgets.QPushButton(Form)
-        self.pushButton_UseProfile.setGeometry(QtCore.QRect(1300, 185, 150, 30))
+        self.pushButton_UseProfile.setGeometry(QtCore.QRect(730, 450, 150, 30))
         self.pushButton_UseProfile.setObjectName("pushButton_UseProfile")
         self.pushButton_UseProfile.setText("Use Profile for Simulation")
+
+        
+        
+        
         #Combobox befüllen mit vorhandenen Daten
         names = list(pd.read_csv("./EMS-Frontend/data/Warmwasser_Profile.csv", usecols = [0], delimiter = ",", encoding='latin1')["Name"])
         self.comboBox_SelectProfile.addItems(names)
+        names = list(pd.read_csv("./EMS-Frontend/data/Warmwasser_Profile_Default.csv", usecols = [0], delimiter = ",", encoding='latin1')["Name"])
+        self.comboBox_DefaultProfile.addItems(names)
         self.comboBox_SelectProfile.activated.connect(self.LoadProfile)
+        self.comboBox_DefaultProfile.activated.connect(self.LoadProfile)
         self.pushButton_SaveProfile.clicked.connect(self.SaveProfile)
         self.pushButton_DeleteProfile.clicked.connect(self.DeleteProfile)
         self.pushButton_UseProfile.clicked.connect(self.UseProfile)
-        
+
+        #Stuff zum schöner machen der Form
+        self.frame_Hourly = QtWidgets.QFrame(Form)
+        self.frame_Hourly.setGeometry(QtCore.QRect(10, 10, 1300, 240))
+        self.frame_Hourly.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_Hourly.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.frame_Hourly.setObjectName("frame_Hourly")
+        self.label_Hourly = QtWidgets.QLabel(Form)
+        self.label_Hourly.setGeometry(QtCore.QRect(20, 8, 200, 30))
+        self.label_Hourly.setObjectName("label_Hourly")
+        self.label_Hourly.setText("Eingabe Profil Stundenwerte")
+
+        self.frame_Monthly = QtWidgets.QFrame(Form)
+        self.frame_Monthly.setGeometry(QtCore.QRect(10, 260, 700, 240))
+        self.frame_Monthly.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_Monthly.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.frame_Monthly.setObjectName("frame_Monthly")
+        self.label_Monthly = QtWidgets.QLabel(Form)
+        self.label_Monthly.setGeometry(QtCore.QRect(20, 255, 200, 30))
+        self.label_Monthly.setObjectName("label_Monthly")
+        self.label_Monthly.setText("Eingabe Profil Monatswerte")
 
 
-        def Create_Slider(i):
+        def Create_Slider_Hourly(i):
             Slider = {}
             verticalSlider = QtWidgets.QSlider(Form)
             verticalSlider.setGeometry(QtCore.QRect(30, 180, 16, 160))
@@ -144,19 +205,15 @@ class Ui_Form(object):
             label.setGeometry(QtCore.QRect(35, 160, 16, 16))
             label.setObjectName("label" + str(i))
 
-  
             spinBox.valueChanged['int'].connect(verticalSlider.setValue) # type: ignore
             verticalSlider.valueChanged['int'].connect(spinBox.setValue) # type: ignore
             spinBox.textChanged.connect(self.PrintSum)
             
             move = 50
-            verticalSlider.move(i * move+30,20)
-            spinBox.move(i * move+20,190)
-            label.move(i * move+33,0)
+            verticalSlider.move(i * move+30,50)
+            spinBox.move(i * move+20,220)
+            label.move(i * move+33,30)
             label.setText(str(i))
-
-
-
 
             Slider["Label"] = label
             Slider["Slider"] = verticalSlider
@@ -166,13 +223,59 @@ class Ui_Form(object):
 
         self.li_Widgets = []
         for i in range(24):
-            self.li_Widgets.append(Create_Slider(i))
+            self.li_Widgets.append(Create_Slider_Hourly(i))
+
+        def Create_Slider_Monthly(i):
+            Slider = {}
+            verticalSlider = QtWidgets.QSlider(Form)
+            verticalSlider.setGeometry(QtCore.QRect(30, 360, 16, 160))
+            verticalSlider.setMaximum(20)
+            verticalSlider.setPageStep(2)
+            verticalSlider.setOrientation(QtCore.Qt.Vertical)
+            verticalSlider.setInvertedAppearance(False)
+            verticalSlider.setObjectName("verticalSlider" + str(i))
+            spinBox = QtWidgets.QSpinBox(Form)
+            spinBox.setGeometry(QtCore.QRect(20, 700, 41, 22))
+            spinBox.setObjectName("spinBox" + str(i))
+            label = QtWidgets.QLabel(Form)
+            label.setGeometry(QtCore.QRect(35, 320, 20, 16))
+            label.setObjectName("label" + str(i))
+
+            spinBox.valueChanged['int'].connect(verticalSlider.setValue) # type: ignore
+            verticalSlider.valueChanged['int'].connect(spinBox.setValue) # type: ignore
+            spinBox.textChanged.connect(self.PrintSum)
+            
+            move = 50
+            verticalSlider.move(i * move+30,20+280)
+            spinBox.move(i * move+20,190+280)
+            label.move(i * move+30,0+280)
+            months = ["Jan","Feb","Mar","Apr","Mai","Jun","Jul","Aug","Sep","Okt","Nov","Dez"]
+            label.setText(months[i])
+
+            Slider["Label"] = label
+            Slider["Slider"] = verticalSlider
+            Slider["SpinBox"] = spinBox
+            
+            return Slider
+        self.li_Widgets_months = []
+        
+        for i in range(12):
+            self.li_Widgets_months.append(Create_Slider_Monthly(i))
+
+
 
     def SaveProfile(self):
-
-        
         names = list(pd.read_csv("./EMS-Frontend/data/Warmwasser_Profile.csv", usecols = [0], delimiter = ",", encoding='latin1')["Name"])
+        names_default = list(pd.read_csv("./EMS-Frontend/data/Warmwasser_Profile_Default.csv", usecols = [0], delimiter = ",", encoding='latin1')["Name"])
+
         name = self.lineEdit_Profil.text() 
+        #Kontrolle ob der Profilname bereits in der Defaultliste vorhanden ist
+        if name in names_default:
+            dlg = QMessageBox()
+            dlg.setWindowTitle("Hinweis")
+            dlg.setText("Profilname kann nicht Defaultname sein!")
+            dlg.exec()
+            return
         
         li_toSave = []
         li_toSave.append(name)
@@ -186,6 +289,8 @@ class Ui_Form(object):
             value = self.doubleSpinBox_WWValue.value()
             li_toSave.append(value)
             li_toSave.append("None")
+        for w in self.li_Widgets_months:
+            li_toSave.append(w["SpinBox"].value())
 
         #Kontrolle ob ein Profil mit diesem Namen bereits existiert
         if name in names:
@@ -195,7 +300,6 @@ class Ui_Form(object):
             writer = csv.writer(f, lineterminator='\n')
             writer.writerow(li_toSave)
         self.UpdateProfiles()
-        #self.LoadProfile()
    
     def DeleteProfile(self):
         name = self.comboBox_SelectProfile.currentText()
@@ -216,16 +320,23 @@ class Ui_Form(object):
                    
 
     def LoadProfile(self):
-        df = pd.read_csv("./EMS-Frontend/data/Warmwasser_Profile.csv", delimiter = ",", encoding='latin1')
-        name = self.comboBox_SelectProfile.currentText()
+        
+        sender = self.sender()
+        name = sender.currentText()
+        if sender.objectName() == "comboBox_DefaultProfile":
+            df = pd.read_csv("./EMS-Frontend/data/Warmwasser_Profile_Default.csv", delimiter = ",", encoding='latin1')
+        elif sender.objectName() == "comboBox_SelectProfile":
+            df = pd.read_csv("./EMS-Frontend/data/Warmwasser_Profile.csv", delimiter = ",", encoding='latin1')
+
+
         self.lineEdit_Profil.setText(name)
         df = df[df.values == name].values.flatten().tolist()
 
-        values = df[1:25]
-        ww_values = df[25:]    
+        values = df[1:25]        
         for i,w in enumerate(self.li_Widgets):
             w["SpinBox"].setValue(values[i])
 
+        ww_values = df[25:27]
         if ww_values[0] != "None":
             self.doubleSpinBox_WWValue.setValue(float(ww_values[0]))
             self.radioButton_m2.setChecked(True)
@@ -233,28 +344,42 @@ class Ui_Form(object):
             self.doubleSpinBox_WWValue.setValue(float(ww_values[1]))
             self.radioButton_Person.setChecked(True)
         
+        values_monthly = df[27:]        
+        for i,w in enumerate(self.li_Widgets_months):
+            w["SpinBox"].setValue(values_monthly[i])
+
     def PrintSum(self):
-        var_sum = 0
+        var_sum_hourly = 0
         for w in self.li_Widgets:
-           var_sum += w["SpinBox"].value()
-        self.lineEdit.setText(str(var_sum) + "%")
-        if var_sum > 100:
-            self.lineEdit.setStyleSheet("QLineEdit"
-                                "{"
-                                "background : red;"
-                                "}")
-        else:
-            self.lineEdit.setStyleSheet("QLineEdit"
-                                "{"
-                                "background : white;"
-                                "}")
-        return var_sum
+           var_sum_hourly += w["SpinBox"].value()
+        self.lineEdit.setText(str(var_sum_hourly) + "%")
+
+        var_sum_monthly = 0
+        for w in self.li_Widgets_months:
+            var_sum_monthly += w["SpinBox"].value()
+        self.lineEdit_sumMonth.setText(str(var_sum_monthly) + "%")
+
+        li_lineEdits = [self.lineEdit,self.lineEdit_sumMonth]
+        li_sums = [var_sum_hourly,var_sum_monthly]
+        for i,it_sum in enumerate(li_sums):
+            if it_sum > 100:
+                li_lineEdits[i].setStyleSheet("QLineEdit"
+                                    "{"
+                                    "background : red;"
+                                    "}")
+                
+            else:
+                li_lineEdits[i].setStyleSheet("QLineEdit"
+                                    "{"
+                                    "background : white;"
+                                    "}")
+        return li_sums
 
     def UseProfile(self):
-        if self.PrintSum() != 100:
+        if any(x != 100 for x in self.PrintSum()):
             dlg = QMessageBox()
             dlg.setWindowTitle("Fehler")
-            dlg.setText("Summe Warmwasserprofil ist nicht 100%")
+            dlg.setText("Summe Warmwasserprofil stündlich oder monatlich ist nicht 100%")
             dlg.exec()
             return
         self.SaveProfile()
