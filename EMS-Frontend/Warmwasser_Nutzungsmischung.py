9b0bf6e22e03245e8f53821b3c482ff7e82c2bb4
marius-coder <64585379+marius-coder@users.noreply.github.com>
2021-12-29 20:05:04 +0100

    Update Warmwasser GUI

--- EMS-Frontend/Warmwasser_Nutzungsmischung.py
+++ EMS-Frontend/Warmwasser_Nutzungsmischung.py
@@ -5,6 +5,7 @@ import os
 import sys
 from bokeh import plotting, embed, resources
 from bokeh.plotting import figure
+from bokeh.models import HoverTool, DataRange1d
 from PyQt5 import *
 #from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
@@ -24,31 +25,72 @@ class WindowGesamtprofil(QWidget):
         # Set tooltips for headers
         self.table.horizontalHeaderItem(0).setToolTip("Column 1 ")
         self.table.horizontalHeaderItem(1).setToolTip("Column 2 ")
+        #Zwischenvariablen
         self.y_hour = []
         self.y_month = []
 
+        #Layout
         self.m_output = QtWebEngineWidgets.QWebEngineView()
         lay = QtWidgets.QVBoxLayout(self)
         lay.addWidget(self.m_output)
         lay.addWidget(self.table)
+
+        #Radiobuttons fürs umschalten zwischen Stunden und Monatsbetrachtung
+        self.radioButton_hour = QtWidgets.QRadioButton(self)
+        self.radioButton_hour.setGeometry(QtCore.QRect(20, 8, 82, 17))
+        self.radioButton_hour.setObjectName("radioButton_hour")
+        self.radioButton_hour.setText("Stunde")
+        self.radioButton_month = QtWidgets.QRadioButton(self)
+        self.radioButton_month.setGeometry(QtCore.QRect(80, 8, 82, 16))
+        self.radioButton_month.setObjectName("radioButton_month")
+        self.radioButton_month.setText("Monat")      
+        self.radioButton_hour.toggled['bool'].connect(self.radioButton_month.repaint)
+        self.radioButton_hour.toggled['bool'].connect(self.UpdatePlot)
+        self.radioButton_month.toggled['bool'].connect(self.radioButton_hour.repaint)
+        self.radioButton_month.toggled['bool'].connect(self.UpdatePlot)
         
-        # prepare some data
-        self.x = np.linspace(1,24,24)
+        #lineEdit in der die Summen angegeben sind zur Kontrolle
+        self.lineEdit_SumNutzung = QtWidgets.QLineEdit(self)
+        self.lineEdit_SumNutzung.setGeometry(QtCore.QRect(160, 8, 50, 20))
+        self.lineEdit_SumNutzung.setReadOnly(True)
 
     def UpdatePlot(self):
         try:
+            if self.radioButton_month.isChecked():
+                y_choose = self.y_month
+                self.x = np.linspace(1,12,12)
+                str_var = "Monat"
+
+            else:
+                y_choose = self.y_hour
+                self.x = np.linspace(1,24,24)
+                str_var = "Stunde"
+
+
+
             self.y = [0 for i in range(24)]
-            print(self.y)
+            sum_Nutzung = 0
             for row in range(self.table.rowCount()): 
-                temp_y = [element * float(self.table.item(row, 1).text()) / 100 for element in self.y_hour[row]]
+                sum_Nutzung += float(self.table.item(row, 1).text())
+                temp_y = [element * float(self.table.item(row, 1).text()) / 100 for element in y_choose[row]]
 
                 self.y = [a + b for a, b in zip(self.y, temp_y)]
 
-            p = figure(title="Simple line example", x_axis_label="x", y_axis_label="y", width=800, height=300)
+            self.lineEdit_SumNutzung.setText(str(sum_Nutzung) + "%")
+            
+            hover_tool = HoverTool(tooltips=[
+            ("x", "@x"),
+            ("y", "$y"),
+                ])  
+            hover_tool.point_policy='snap_to_data'
+  
+            p = figure(title="Gesamtprofil " + str_var, x_axis_label="Stunde", y_axis_label="Verbrauch [%]", width=800, height=300,
+                       tools=[hover_tool,"pan,wheel_zoom,box_zoom,reset,save"], y_range=DataRange1d(start=0))
 
+          
             # add a line renderer with legend and line thickness
-            p.line(self.x, self.y, legend_label="Temp.", line_width=2)
-        
+            p.line(self.x, self.y, line_width=2)
+            #p.legend.visible = False
         
             html = embed.file_html(p, resources.CDN, "my plot")
             self.m_output.setHtml(html)
